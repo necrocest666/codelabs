f1af758c4b4decf5d765915054475318b8d61bd1
necrocest666 <37267711+necrocest666@users.noreply.github.com>
2022-04-15 18:21:14 +0500

    forked for practice

--- firebase-get-to-know-flutter/step_02/lib/main.dart
+++ firebase-get-to-know-flutter/step_02/lib/main.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+import 'src/authentication.dart';
+
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'src/widgets.dart';
 
+enum Attending { yes, no, unknown }
 void main() {
-  runApp(App());
+  runApp(ChangeNotifierProvider(
+      //listen to changes in create
+      create: ((context) => ApplicationState()),
+      //respond to changes in create
+      builder: ((context, _) => App())));
 }
 
 class App extends StatelessWidget {
@@ -42,6 +56,19 @@ class HomePage extends StatelessWidget {
           const SizedBox(height: 8),
           const IconAndDetail(Icons.calendar_today, 'October 30'),
           const IconAndDetail(Icons.location_city, 'San Francisco'),
+          //notifyListener() consumed by the Consumer
+          Consumer<ApplicationState>(
+              builder: (context, appState, _) => Authentication(
+                  loginState: appState.loginState,
+                  email: appState.email,
+                  startLoginFlow: appState.startLoginFlow,
+                  verifyEmail: appState.verifyEmail,
+                  signInWithEmailAndPassword:
+                      appState.signInWithEmailAndPassword,
+                  cancelRegistration: appState.cancelRegistration,
+                  registerAccount: appState.registerAccount,
+                  signOut: appState.signOut)),
+
           const Divider(
             height: 8,
             thickness: 1,
@@ -53,8 +80,365 @@ class HomePage extends StatelessWidget {
           const Paragraph(
             'Join us for a day full of Firebase Workshops and Pizza!',
           ),
+
+          Consumer<ApplicationState>(
+              builder: (context, appState, _) => Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      if (appState.attendees >= 2)
+                        Paragraph('${appState.attendees} people going')
+                      else if (appState.attendees == 1)
+                        const Paragraph('1 person going')
+                      else
+                        const Paragraph('No one going'),
+                      if (appState.loginState ==
+                          ApplicationLoginState.loggedIn) ...[
+                        //the tiple dot (spread operator) joins the below list of widgets to the current one of children
+                        YesNoSelection(
+                          appState.attending,
+                          (attending) => appState.attending = attending,
+                        ),
+
+                        const Header('Discussion'),
+                        GuestBook(
+                            (message) =>
+                                appState.addMessageToGuestBook(message),
+                            appState.guestBookMessages),
+                      ]
+                    ],
+                  )),
         ],
       ),
     );
+  }
+}
+
+class ApplicationState extends ChangeNotifier {
+  ApplicationState() {
+    init();
+  }
+
+  Future<void> init() async {
+    //initiallize firebase
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    //check attendees
+    FirebaseFirestore.instance
+        .collection('attendees')
+        .where('attending', isEqualTo: true)
+        .snapshots()
+        .listen((snapshot) {
+      _attendees = snapshot.docs.length;
+      notifyListeners();
+    });
+
+    //instance listens for changes in user login state
+    //callback called when the user's login state changes
+    FirebaseAuth.instance.userChanges().listen((user) {
+      if (user != null) {
+        _loginState = ApplicationLoginState.loggedIn;
+        _guestBookSubscription = FirebaseFirestore.instance
+            .collection('guestbook')
+            .orderBy('timestamp', descending: true)
+            .snapshots()
+            .listen((snapshot) {
+          _guestBookMessages = [];
+          //whenever any change occurs recreate the date from scratch instead of compensating for every possible state change
+          for (final document in snapshot.docs) {
+            _guestBookMessages.add(
+              GuestBookMessage(
+                document.data()['name']
+                    as String, //returned data is of type dyanmic but class only accepts String so cast data as String
+                document.data()['text'] as String,
+              ),
+            );
+          }
+          notifyListeners();
+        });
+        _attendingSubscription = FirebaseFirestore.instance
+            .collection('attendees')
+            .doc(user.uid)
+            .snapshots()
+            .listen((snapshot) {
+          if (snapshot.data() != null) {
+            if (snapshot.data()!['attending'] as bool) {
+              _attending = Attending.yes;
+            } else {
+              _attending = Attending.no;
+            }
+          } else {
+            _attending = Attending.unknown;
+          }
+          notifyListeners();
+        });
+      } else {
+        _loginState = ApplicationLoginState.loggedOut;
+        _guestBookMessages = []; //wipe the messages
+        _guestBookSubscription
+            ?.cancel(); //remove listener when it no longet needed
+        _attendingSubscription?.cancel();
+      }
+      //notify listeners in order to change application state
+      notifyListeners();
+    });
+  }
+
+  //private variables need a getter
+  ApplicationLoginState _loginState = ApplicationLoginState.loggedOut;
+  ApplicationLoginState get loginState => _loginState;
+
+  String? _email;
+  String? get email => _email;
+
+  StreamSubscription<QuerySnapshot>? _guestBookSubscription;
+  List<GuestBookMessage> _guestBookMessages = [];
+  List<GuestBookMessage> get guestBookMessages => _guestBookMessages;
+
+  int _attendees = 0;
+  int get attendees => _attendees;
+
+  Attending _attending = Attending.unknown;
+  StreamSubscription<DocumentSnapshot>? _attendingSubscription;
+  Attending get attending => _attending;
+
+  set attending(Attending attending) {
+    final userDoc = FirebaseFirestore.instance
+        .collection('attendees')
+        .doc(FirebaseAuth.instance.currentUser!.uid);
+    if (attending == Attending.yes) {
+      userDoc.set(<String, dynamic>{'attending': true});
+    } else {
+      userDoc.set(<String, dynamic>{'attending': false});
+    }
+  }
+
+  //start login flow at the email address state
+  void startLoginFlow() {
+    _loginState = ApplicationLoginState.emailAddress;
+    notifyListeners();
+  }
+
+  Future<void> verifyEmail(
+    String email,
+    void Function(FirebaseAuthException e) errorCallback,
+  ) async {
+    try {
+      //check existence of email by seeing possible associated sign in methods with that email
+      var methods =
+          await FirebaseAuth.instance.fetchSignInMethodsForEmail(email);
+      if (methods.contains('password')) {
+        //if a matching password found,
+        _loginState = ApplicationLoginState.password; //shift to pwd screen
+      } else {
+        _loginState = ApplicationLoginState.register; //else to register
+      }
+      _email = email;
+      notifyListeners();
+    } on FirebaseAuthException catch (e) {
+      errorCallback(e);
+    }
+  }
+
+  void signInWithEmailAndPassword(
+    String email,
+    String password,
+    void Function(FirebaseAuthException e) errorCallback,
+  ) async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      errorCallback(e);
+    }
+  }
+
+  void cancelRegistration() {
+    _loginState = ApplicationLoginState
+        .emailAddress; //cancel process and move back to emailaddress screen
+    notifyListeners();
+  }
+
+  void registerAccount(String email, String displayName, String password,
+      void Function(FirebaseAuthException e) errorCallback) async {
+    try {
+      var credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      //another call is done to update the user's displayName
+      //name stored along with auth object
+      await credential.user!.updateDisplayName(displayName);
+    } on FirebaseAuthException catch (e) {
+      errorCallback(e);
+    }
+  }
+
+  void signOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
+  Future<DocumentReference> addMessageToGuestBook(String message) {
+    //check login
+    if (_loginState != ApplicationLoginState.loggedIn) {
+      throw Exception('Must be logged in');
+    }
+
+    //add to collection
+    return FirebaseFirestore.instance
+        .collection('guestbook')
+        .add(<String, dynamic>{
+      //type added because need to cast data as explicit type with relevant maps
+      'text': message,
+      'timestamp': DateTime.now().millisecondsSinceEpoch,
+      'name': FirebaseAuth.instance.currentUser!.displayName,
+      'userId': FirebaseAuth.instance.currentUser!.uid,
+    });
+  }
+}
+
+class GuestBook extends StatefulWidget {
+  const GuestBook(
+      this.addMessage, this.messages); //function called when user clicks Send
+  final FutureOr<void> Function(String message) addMessage;
+  final List<GuestBookMessage> messages;
+
+  @override
+  State<GuestBook> createState() => _GuestBookState();
+}
+
+class _GuestBookState extends State<GuestBook> {
+  final _formKey = GlobalKey<FormState>(debugLabel: '_GuestBookState');
+  final _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Form(
+            key: _formKey,
+            child: Row(
+              children: [
+                //expanded widgert so text form takes up entire width
+                Expanded(
+                  child: TextFormField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      hintText: 'Leave a message',
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Enter message tp continue';
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                StyledButton(
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      await widget.addMessage(_controller.text);
+                      _controller.clear();
+                    }
+                  },
+                  child: Row(children: const [
+                    Icon(Icons.send),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Text("SEND"),
+                  ]),
+                ),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        for (var message in widget.messages)
+          Paragraph('${message.name}: ${message.message}'),
+        const SizedBox(
+          height: 8,
+        )
+      ],
+    );
+  }
+}
+
+class GuestBookMessage {
+  GuestBookMessage(this.name, this.message);
+  final String name;
+  final String message;
+}
+
+class YesNoSelection extends StatelessWidget {
+  const YesNoSelection(this.state, this.onSelection);
+  final Attending state;
+  final void Function(Attending selection) onSelection;
+
+  @override
+  Widget build(BuildContext context) {
+    switch (state) {
+      case Attending.yes:
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(elevation: 0),
+                onPressed: () => onSelection(Attending.yes),
+                child: const Text('YES'),
+              ),
+              const SizedBox(width: 8),
+              TextButton(
+                onPressed: () => onSelection(Attending.no),
+                child: const Text('NO'),
+              ),
+            ],
+          ),
+        );
+      case Attending.no:
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              TextButton(
+                onPressed: () => onSelection(Attending.yes),
+                child: const Text('YES'),
+              ),
+              const SizedBox(width: 8),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(elevation: 0),
+                onPressed: () => onSelection(Attending.no),
+                child: const Text('NO'),
+              ),
+            ],
+          ),
+        );
+      default:
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              StyledButton(
+                onPressed: () => onSelection(Attending.yes),
+                child: const Text('YES'),
+              ),
+              const SizedBox(width: 8),
+              StyledButton(
+                onPressed: () => onSelection(Attending.no),
+                child: const Text('NO'),
+              ),
+            ],
+          ),
+        );
+    }
   }
 }
